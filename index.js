1aa1f65dfabc9f6a55cdde5b3fb2053f01aef6e9
bgsharath <sharathbg26@gmail.com>
2025-02-02 20:13:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Sample Route
+app.get("/", (req, res) => {
+  res.send("Hello, World! 🌍🚀");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
